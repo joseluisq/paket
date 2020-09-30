9f572df222bea8b1e29ffcab018fab65ce05699a
Jose Quintana <joseluisquintana20@gmail.com>
2020-10-01 00:12:51 +0200

    chore: impl read_pkg_file config function

--- src/helpers/file.rs
+++ src/helpers/file.rs
@@ -1,4 +1,7 @@
+use std::fs;
 use std::path::{Path, PathBuf};
+
+use crate::result::{Context, Result};
 
 pub fn has_path_any_suffixes<P: AsRef<Path>>(path: P, suffixes: &[&str]) -> bool
 where
@@ -6,4 +9,15 @@ where
 {
     let path = PathBuf::from(path);
     suffixes.iter().any(|x| path.ends_with(x))
+}
+
+pub fn read(path: &Path) -> Result<String> {
+    match String::from_utf8(read_bytes(path)?) {
+        Ok(s) => Ok(s),
+        Err(_) => bail!("path at `{}` was not valid utf-8", path.display()),
+    }
+}
+
+pub fn read_bytes(path: &Path) -> Result<Vec<u8>> {
+    fs::read(path).with_context(|| format!("failed to read `{}`", path.display()))
 }
--- src/pkg/conf/mod.rs
+++ src/pkg/conf/mod.rs
@@ -1,3 +1,38 @@
+use serde::Deserialize;
+use std::path::Path;
+
+use crate::helpers::file;
+use crate::result::Result;
+
 pub mod conf;
 
 pub use conf::*;
+
+pub fn read_pkg_file(path: &Path) -> Result<toml::Value> {
+    let toml = file::read(path)?;
+
+    let first_error = match toml.parse() {
+        Ok(ret) => return Ok(ret),
+        Err(e) => e,
+    };
+
+    let mut second_parser = toml::de::Deserializer::new(&toml);
+    second_parser.set_require_newline_after_table(false);
+    if let Ok(ret) = toml::Value::deserialize(&mut second_parser) {
+        let msg = format!(
+            "\
+TOML file found which contains invalid syntax and will soon not parse
+at `{}`.
+The TOML spec requires newlines after table definitions (e.g., `[a] b = 1` is
+invalid), but this file has a table header which does not have a newline after
+it. A newline needs to be added and this warning will soon become a hard error
+in the future.",
+            path.display()
+        );
+        println!("{}", &msg);
+        return Ok(ret);
+    }
+
+    let first_error = anyhow::Error::from(first_error);
+    Err(first_error.context("could not parse input as TOML"))
+}
